f3a43d6feed2bf5a59f545bae1f48901ce1183cb
Laraib481 <laraib15869@gmail.com>
2026-08-27 16:15:45 +0500

    new modify

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -5,10 +5,16 @@
 
 // module.exports = app;
 
-const app = require('../src/app');
-const connectDb = require('../src/db/db');
 
-module.exports = async (req, res) => {
-  await connectDb();
-  return app(req, res);
-};
+
+// const app = require('../src/app');
+// const connectDb = require('../src/db/db');
+
+// module.exports = async (req, res) => {
+//   await connectDb();
+//   return app(req, res);
+// };
+
+const app = require('../src/app');
+
+module.exports = app;
