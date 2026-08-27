ace8e139ff23873c70ae957a5f48b132562d3dd2
Laraib481 <laraib15869@gmail.com>
2026-08-27 16:03:12 +0500

    modify db.js

---
+++ Backend/api/index.js
@@ -0,0 +1,14 @@
+// const app = require('../src/app');
+// const connectDb = require('../src/db/db');
+
+// connectDb();
+
+// module.exports = app;
+
+const app = require('../src/app');
+const connectDb = require('../src/db/db');
+
+module.exports = async (req, res) => {
+  await connectDb();
+  return app(req, res);
+};
--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,18 +1,35 @@
+// const mongoose = require('mongoose');
+
+// async function connectDb(){
+   
+//    try{
+
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log('connect db successfully');
+     
+//    }catch(err){
+//     console.log("database connection err", err);
+//    }
+
+// }
+
+
+
+// module.exports = connectDb;
+
 const mongoose = require('mongoose');
 
-async function connectDb(){
-   
-   try{
+let isConnected = false;
 
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('connect db successfully');
-     
-   }catch(err){
-    console.log("database connection err", err);
-   }
+async function connectDb() {
+  if (isConnected && mongoose.connection.readyState === 1) {
+    return;
+  }
 
+  await mongoose.connect(process.env.MONGO_URI);
+
+  isConnected = true;
+  console.log('MongoDB connected successfully');
 }
-
-
 
 module.exports = connectDb;
